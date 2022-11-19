4f5d24f0bd6ffbfdb926953570fa68d5aa7f3c29
Chi Thu Le <chithu.le@systemverification.com>
2022-11-19 09:45:38 +0100

    Update cypress version

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -2,6 +2,8 @@ describe('test redirect', () => {
   it('it', () => {
     cy.visit('https://localhost:3000');
     cy.get('button').click();
-    cy.get('body').contains('is present')
+    cy.origin('https://oauth-simulator.mocklab.io', () => {
+      cy.get('body').contains('The XSRF-TOKEN is pressent')
+    })
   });
 });
